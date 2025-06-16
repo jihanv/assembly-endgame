ae753e806cfed6a8625711c8390e0e4d051b4e38
jihanv <hedjmny@gmail.com>
2025-06-16 19:12:30 +0900

    Add game status section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ export default function AssemblyEndgame() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly</p>
       </header>
+      <section className="game-status" >
+        <h2>You win!</h2>
+        <p>Well done!</p>
+      </section>
 
     </main>
   )
