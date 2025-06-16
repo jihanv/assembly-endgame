f79d6f0d74f47ba899607ee983e6abae3c9b5f99
jihanv <hedjmny@gmail.com>
2025-06-16 19:48:11 +0900

    Add language section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 import './App.css'
-
+import { languages } from "./languages.js"
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Create the language chips. Use the
+ * `languages.js` file to pull in the array of
+ * languages to use, which contains the language
+ * name, background color, and text color.
+ * 
+ * Hint for layout: use a flex container that can wrap
+ * to layout the languages.
+ */
 export default function AssemblyEndgame() {
+  const languageList = languages
   return (
     <main>
       <header>
@@ -12,7 +24,11 @@ export default function AssemblyEndgame() {
         <h2>You win!</h2>
         <p>Well done!</p>
       </section>
-
+      <section className="languages">
+        {languageList.map(language => <span key={language.name} className="language" style={{
+          backgroundColor:language.backgroundColor,
+          color: language.color}}>{language.name}</span>)}
+      </section>
     </main>
   )
 }
