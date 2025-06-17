b045dbc67abf6b86a019b06087c060edc5d83baf
jihanv <hedjmny@gmail.com>
2025-06-17 19:16:29 +0900

    Refactor languageList

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import { languages } from "./languages.js"
 /**
  * Goal: Build out the main parts of our app
  * 
- * Challenge: Create the language chips. Use the
- * `languages.js` file to pull in the array of
- * languages to use, which contains the language
- * name, background color, and text color.
- * 
- * Hint for layout: use a flex container that can wrap
- * to layout the languages.
+ * Challenge: 
+ * 1. Save a "currentWord" in state. Initialize as "react".
+ * 2. Map over the letters of the word (you'll need to turn 
+ *    the string into an array of letters first) and display
+ *    each one as a <span>. Capitalize the letters when
+ *    displaying them.
+ * 3. Style to look like the design. You can get the underline 
+ *    effect on the box using `border-bottom`.
  */
 export default function AssemblyEndgame() {
-  const languageList = languages
+  const languageList = languages.map(language => <span key={language.name} className="language" style={{
+          backgroundColor:language.backgroundColor,
+          color: language.color}}>{language.name}</span>)
   return (
     <main>
       <header>
@@ -25,9 +28,7 @@ export default function AssemblyEndgame() {
         <p>Well done!</p>
       </section>
       <section className="languages">
-        {languageList.map(language => <span key={language.name} className="language" style={{
-          backgroundColor:language.backgroundColor,
-          color: language.color}}>{language.name}</span>)}
+        {languageList}
       </section>
     </main>
   )
