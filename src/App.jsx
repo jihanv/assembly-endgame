7686648d4fb042e57e88c3e547e7b378410b9923
jihanv <hedjmny@gmail.com>
2025-06-18 20:18:49 +0900

    Fixed <p> centering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import "./App.css";
 import { languages } from "./languages.js";
 import { useState } from "react";
-/**
- * Goal: Build out the main parts of our app
- *
- * Challenge:
- * 2. Map over the letters of the word (you'll need to turn
- *    the string into an array of letters first) and display
- *    each one as a <span>. Capitalize the letters when
- *    displaying them.
- * 3. Style to look like the design. You can get the underline
- *    effect on the box using `border-bottom`.
- */
 
 export default function AssemblyEndgame() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -48,13 +37,12 @@ export default function AssemblyEndgame() {
       <header>
         <h1>Assembly: Endgame</h1>
         <p>
-          Guess the word in under 8 attempts to keep the programming world safe
-          from Assembly
+          Guess the word in under 8 attempts to keep the programming world safe from Assembly
         </p>
       </header>
       <section className="game-status">
         <h2>You win!</h2>
-        <p>Well done!</p>
+        <p>Well done! 🎉</p>
       </section>
       <section className="languages">{languageList}</section>
       <section className="word">
