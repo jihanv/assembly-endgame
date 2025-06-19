c4efb914bc7abb50e5187006cdec5e56bd12346a
jihanv <hedjmny@gmail.com>
2025-06-19 20:35:00 +0900

    Update the keyboard color depending on whether the letter is right or wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
 import "./App.css";
 import { languages } from "./languages.js";
 import { useState } from "react";
+import { clsx } from "clsx";
 
 export default function AssemblyEndgame() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   const [currentWord, setCurrentWord] = useState("react");
 
-  const [guessedLetters, setGuessedLetters] = useState([])
-
-  console.log(guessedLetters)
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
   function addGuessedLetter(letter) {
-    setGuessedLetters(prevLetters => 
-      prevLetters.includes(letter)? prevLetters : [...prevLetters, letter]
-    )
-    console.log(guessedLetters)
+    let inWord = false;
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+    if (currentWord.split("").includes(letter.toLowerCase())) {
+      inWord = true;
+    }
+    changeKeyboard(letter, inWord);
   }
 
+  function changeKeyboard(letter, inWord) {
+    console.log("Changing keyboard");
+    const newButtonClasses = clsx({
+      key: true,
+      correct: inWord,
+      incorrect: !inWord,
+    });
+    console.log(newButtonClasses);
+    setPressedKey((prevKeyboard) => {
+      const newkyboard = prevKeyboard.map((key) => {
+        if (key.props["data-character"] === letter.toUpperCase()) {
+          return (
+            <button
+              onClick={() => addGuessedLetter(key)}
+              key={key.index}
+              className={newButtonClasses}
+              data-character={letter.toUpperCase()}
+            >
+              {letter.toUpperCase()}
+            </button>
+          );
+        }
+        return key;
+      });
+      return newkyboard;
+    });
+  }
   const languageList = languages.map((language) => (
     <span
       key={language.name}
@@ -35,20 +65,30 @@ export default function AssemblyEndgame() {
     <span key={index} className="letter">
       {letter.toUpperCase()}
     </span>
-  ))
-
-  const keys = alphabet.split("").map((key, index) => (
-    <button onClick={() => addGuessedLetter(key)} key={index} className="key">
-      {key.toUpperCase()}
-    </button>
   ));
 
+  const keys = alphabet.split("").map((key, index) => {
+    const buttonClass = clsx("key");
+    return (
+      <button
+        onClick={() => addGuessedLetter(key)}
+        key={index}
+        className={buttonClass}
+        data-character={key.toUpperCase()}
+      >
+        {key.toUpperCase()}
+      </button>
+    );
+  });
+
+  const [pressedKey, setPressedKey] = useState(keys);
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>
-          Guess the word in under 8 attempts to keep the programming world safe from Assembly
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly
         </p>
       </header>
       <section className="game-status">
@@ -56,12 +96,8 @@ export default function AssemblyEndgame() {
         <p>Well done! 🎉</p>
       </section>
       <section className="languages">{languageList}</section>
-      <section className="word">
-        {letterElements}
-      </section>
-      <section className="keyboard">
-        {keys}
-      </section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{pressedKey}</section>
       <button className="new-game">New Game</button>
     </main>
   );
